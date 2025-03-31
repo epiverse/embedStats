2f8c04667851a3d8a0b2ce437fe7bc65ab4f303a
Jonas Almeida <jalmeida@mathbiol.org>
2025-03-31 16:56:53 -0400

    load local file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log(`embedStats index.js initiatized at \n${Date()}`);
+
+//const JSZip = await import('https://esm.sh/jszip@3.10.1').default
+
+(async function() {
+    // load dependencies
+    const JSZip = (await import('https://esm.sh/jszip@3.10.1')).default
+    // ...
+    loadLocal.onclick = function() {
+        console.log('JSZip',JSZip)
+    }
+})()
