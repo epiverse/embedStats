4dbe7c043a383bebeb9783548130ee4b243e52bd
Jonas Almeida <jalmeida@mathbiol.org>
2025-05-20 17:13:29 -0400

    json->tsv

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 console.log(`embedStats index.js initiatized at \n${Date()}`);
 
-//const JSZip = await import('https://esm.sh/jszip@3.10.1').default
 
 (async function() { // keeping interactions within annonymous async function :-P
     
     // load dependencies
-    const JSZip = (await import('https://esm.sh/jszip@3.10.1')).default
+    const JSZip = await import('https://esm.sh/jszip@3.10.1').default
+    const embedStats = await import('./embedStats.mjs')
     
-    // ...
-    loadLocal.onclick = function() {
-        console.log('Load local file',JSZip)
+    // setting the stage
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
+    
+    await delay(1000)
+    loadRemoteVectorsInput.hidden=true
+    loadRemoteMetaInput.hidden=true
+    
+    // set onclick open/close url input
+    loadRemoteVectors.onclick=async function(){
+        // loadRemoteVectorsInput.hidden=!loadRemoteVectorsInput.hidden
+        if(loadRemoteVectorsInput.hidden){ // input is close, open it
+            loadRemoteVectorsInput.hidden=false
+            loadRemoteVectorsInput.value='https://raw.githubusercontent.com/epiverse/pathembed/refs/heads/main/tcgaPathReports.json.zip'
+        }else{ // input is open, load URL
+            //let vectors = await embedStats.loadZippedFile(loadRemoteVectorsInput.value)
+            let docs = await (await import('./embedStats.mjs')).unzipURL(loadRemoteVectorsInput.value)
+            let vectors = docs.map(x=>x.embeddings).map(x=>x.join('\t'))
+            debugger
+        }
+    }
+    loadRemoteMeta.onclick=function(){
+        loadRemoteMetaInput.hidden=!loadRemoteMetaInput.hidden
+    }
+
+    
 })()
